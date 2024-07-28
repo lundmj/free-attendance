78a07caccb5c828157e66737cfcab54d1a5811da
Matthew Lund <lundmatthewj@gmail.com>
2024-07-28 10:14:45 -0600

    Create basic index page

--- attendance/views.py
+++ attendance/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.views import View
 
-# Create your views here.
+class IndexView(View):
+    template_name = "attendance/index.html"
+    def get(self, request):
+        return render(request, "attendance/index.html")
+
